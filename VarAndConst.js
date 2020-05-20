33d63ebf2f46deaf97e729562465e51779ea5252
priyanksondagar <32874404+priyanksondagar@users.noreply.github.com>
2020-05-20 22:00:31 +0530

    variable and constant file

---
+++ VarAndConst.js
@@ -0,0 +1,19 @@
+//variable 
+
+let i="i am variable"
+console.log(i)
+
+var j='i am new variable'
+console.log(j)
+
+//value of j can be change
+var j='i am newest variable'
+console.log(j)
+
+//constant 
+const p="i am constant"
+console.log(p)
+
+//constant can not be change
+const p="i am chenged constant"
+console.log(p)
