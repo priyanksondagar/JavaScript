6ca4e4a9ec40db99ea4de630656521cb3a2c3648
priyanksondagar <32874404+priyanksondagar@users.noreply.github.com>
2020-05-21 15:55:25 +0530

    logicalOperator program fiel

---
+++ LogicalOperator.js
@@ -0,0 +1,20 @@
+//logical and operator
+let Username='priyank4820'
+let Password='priyank123'
+let Username1='priyank4820'
+let Password1='priyank123'
+if(Username1==Username&&Password1==Password)
+{
+    console.log("loggin successfully")
+}
+
+//logical or operator
+let Uname='priyank4820'
+let Pass='priyank123'
+let Uname1='priyank4820'
+let Pass1='priyank'
+if(Uname1==Uname1 || Pass1==Pass)
+{
+    console.log("Username or password is incorrect")
+}
+
