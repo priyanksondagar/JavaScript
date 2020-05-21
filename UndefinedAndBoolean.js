566189d0460cc2c1b3c4f9e3c5491996540b3ca6
priyanksondagar <32874404+priyanksondagar@users.noreply.github.com>
2020-05-21 15:57:04 +0530

    undefinedAndBoolean program file

---
+++ UndefinedAndBoolean.js
@@ -0,0 +1,17 @@
+//undefined
+let i
+console.log(i)
+console.log(typeof i)
+
+//boolean
+let j=true
+if(j)
+{
+    console.log("i'm true")
+}
+
+let k=false
+if(k)
+{
+    console.log("i will not display bcz of false")
+}
