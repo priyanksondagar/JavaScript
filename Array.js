f3cde8b21739f166e57e9b60b16fd2d4ee16c210
priyanksondagar <32874404+priyanksondagar@users.noreply.github.com>
2020-05-21 15:53:22 +0530

    Array file

---
+++ Array.js
@@ -0,0 +1,9 @@
+let AvailableFlat=[101,102,201,202,301,302,401,402,501,502]
+console.log(AvailableFlat)
+
+//find first flat of 2nd floor
+console.log(AvailableFlat[2])
+
+console.log('size of array : '+AvailableFlat.length)
+//find last available flat
+console.log(AvailableFlat[AvailableFlat.length-1])
