d4c2b929d09b91eb8179fd3f111301984f3019a4
priyanksondagar <32874404+priyanksondagar@users.noreply.github.com>
2020-05-21 15:59:54 +0530

    number datatype's program file

---
+++ numbers.js
@@ -0,0 +1,13 @@
+//number defined without comma
+let number=100
+console.log(number)
+console.log(typeof(number))
+
+//Finding a cube 
+var length=100;
+var width=100;
+var height=100;
+
+var cube=length*width*height;
+
+console.log(cube)
