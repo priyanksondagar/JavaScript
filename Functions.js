d6e5cc4cf9532c45a2d9d21d4ed8befcd52b05b1
priyanksondagar <32874404+priyanksondagar@users.noreply.github.com>
2020-05-22 19:22:39 +0530

    user defined function

---
+++ Functions.js
@@ -0,0 +1,13 @@
+var getUser=function(name,gender){
+    if(gender=='m')
+    { 
+    console.log("Welcome Mr."+name)
+    }
+    else if(gender=='f')
+    {
+        console.log("Welcome Mrs."+name)
+    }
+}
+
+getUser('Priyank','m')
+getUser('Riddhi','f')
