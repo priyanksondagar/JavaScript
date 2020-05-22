8b35a664719d6e618ff1a2b0e7203eafd90fcb7a
priyanksondagar <32874404+priyanksondagar@users.noreply.github.com>
2020-05-22 19:21:48 +0530

    For loop program

---
+++ ForLoop.js
@@ -0,0 +1,7 @@
+let students=['priyank','dhruv','darshit','riddhi','janvi']
+
+console.log('List of students : ')
+for(let i=0;i<students.length;i++)
+{
+    console.log("Name: "+students[i]+"      Rank: "+(i+1))
+}
