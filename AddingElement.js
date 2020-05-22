ffc144318f0dc23b716c1631e6d040f760f44e01
priyanksondagar <32874404+priyanksondagar@users.noreply.github.com>
2020-05-22 19:19:43 +0530

    adding element in array

---
+++ AddingElement.js
@@ -0,0 +1,11 @@
+let student=[]
+
+student.unshift('priyank')
+student.unshift('dhruv')
+student.unshift('riddhi')
+
+console.log("students are : ")
+for(let i=0;i<student.length;i++)
+{
+    console.log(student[i])
+}
