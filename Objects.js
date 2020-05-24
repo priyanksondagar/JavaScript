a9b650999ff121fb83f31aadf264747b473af1b1
priyanksondagar <32874404+priyanksondagar@users.noreply.github.com>
2020-05-24 22:29:27 +0530

    objects in a javaScript

---
+++ Objects.js
@@ -0,0 +1,16 @@
+let student1={
+    name:'priyank',
+    gender:'male',
+    enroll:51,
+}
+
+let student2={
+    name:'riddhi',
+    gender:'female',
+    enroll:43
+}
+
+console.log(student2)
+console.log(student2.name)
+console.log(student1.enroll)
+console.log(`hey ${student1.name} and ${student2.name} you both are awesome !!`)
