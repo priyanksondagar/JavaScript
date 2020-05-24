4c48afcbdc2e5a9d51fbc29ba102f19d92373dc4
priyanksondagar <32874404+priyanksondagar@users.noreply.github.com>
2020-05-24 22:45:55 +0530

    random number generator

---
+++ Random.js
@@ -0,0 +1,13 @@
+//random method
+
+console.log(Math.random())
+// it gives ny number between 0 and 1
+
+//floor function
+let i=3.2
+console.log(Math.floor(i))
+// it gives maximum value comes before original value
+
+//ceil function
+console.log(Math.ceil(i))
+//it gives minimum value comes after original value
