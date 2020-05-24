ef31f5bdc3e2eb2de7799f6f1cb8222b9ce164d2
priyanksondagar <32874404+priyanksondagar@users.noreply.github.com>
2020-05-24 22:31:19 +0530

    use of array function

---
+++ ArrayFunction.js
@@ -0,0 +1,14 @@
+//simple fuction
+
+let student=['priyank','dhruv','darshit','riddhi','krishna']
+
+student.forEach(function(name){
+    console.log(name)
+})
+
+console.log('New students : ')
+//arrow function
+
+let student1=['priya','dhruvi','darshi','ridzzi']
+
+student1.forEach(name=>console.log(name))
